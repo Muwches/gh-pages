b1c63a9437cdfdce74ea490842faa6919b7bd7ef
Muwches <muwches11@gmail.com>
2018-10-27 00:49:28 -0500

    Create HelloWorld.php

---
+++ HelloWorld.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Checking Bootstrap</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css.map">
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<?php
+
+<h2>Hello world!!</h2>
+
+?>
+</body>
+</html>
